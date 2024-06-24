92e8b0f432c4ed4fd7764456f0882229bea46285
The-EMAR <mesut.nedzhib@gmail.com>
2024-06-24 20:21:17 +0300

    add custom product media slider

---
+++ assets/main-product-media-slider.js
@@ -0,0 +1,34 @@
+(() => {
+  if (customElements.get('product-media-slider')) {
+    return;
+  }
+
+  class ProductMediaSlider extends HTMLElement {
+    constructor() {
+      super();
+      this.slider = null;
+      this.options = {
+        waitForTransition: true,
+        perPage: 1,
+        pagination: true,
+        gap: '2rem',
+        arrows: false
+      };
+    }
+
+    connectedCallback() {
+      this.initSlider();
+    }
+
+    initSlider() {
+      if (!this.querySelector('.js-slider')) return;
+
+      this.slider = new Splide(
+        this.querySelector('.js-slider'),
+        this.options
+      ).mount();
+    }
+  }
+
+  customElements.define('product-media-slider', ProductMediaSlider);
+})();
